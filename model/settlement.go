4a0e2511dd584db1e3a863620e419a33c4ee83a2
a.siahmansouri <a.siahmansouri@rightel.ir>
2025-08-03 10:27:53 +0330

    modelupdate

---
+++ model/settlement.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// SettleTradeEvent is the event sent from matching engine to settlement service for trade settlement.
+type SettleTradeEvent struct {
+	EventID       uuid.UUID `json:"event_id"` // For idempotency & audit
+	Version       int       `json:"version"`  // For backward/forward compatibility
+	TakerOrderID  uuid.UUID `json:"taker_order_id"`
+	MakerOrderID  uuid.UUID `json:"maker_order_id"`
+	MatchAmount   float64   `json:"match_amount"`             // Amount of asset matched
+	TradePrice    float64   `json:"trade_price"`              // Matched price
+	TraceID       string    `json:"trace_id,omitempty"`       // For distributed tracing (optional)
+	CorrelationID string    `json:"correlation_id,omitempty"` // For cross-service tracing (optional)
+	CreatedAt     time.Time `json:"created_at"`               // UTC time of event creation
+	// Future fields:
+	// Metadata      map[string]string `json:"metadata,omitempty"`
+}
